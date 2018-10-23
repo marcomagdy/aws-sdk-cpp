dd024935655b6d4e8484039b7f6f5b9ea4104016
Marco Magdy <magmarco@9801a79ea129.ant.amazon.com>
2018-10-23 09:41:21 -0700

    Add a RaspberryPi CMake toolchain file
    You need to have a sysroot directory from the Raspberry Pi SDK on your
    host machine.
    Then with this toolchain you can run the following command to
    cross-compile the SDK:
    
    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=toolchains/raspberrypi.toolchain.cmake \
    -DCMAKE_SYSROOT=<sysroot path here> -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
    -DTARGET_ARCH=LINUX

---
+++ toolchains/raspberrypi.toolchain.cmake
@@ -0,0 +1,23 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(triple armv7m-unknown-linux-gnueabihf)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld")
+set(CMAKE_STATIC_LINKER_FLAGS "-fuse-ld=lld")
+set(CMAKE_EXE_LINKER_FLAGS  "-fuse-ld=lld")
+# rsync that comes with macOS Sierra 10.12 does not support multiple remote sources. Using the rsync installed via brew install rsync however does.
+# mkdir sysroot
+# rsync -rzLR --safe-links \
+#       pi@raspberrypi:/usr/lib/arm-linux-gnueabihf \
+#       pi@raspberrypi:/usr/lib/gcc/arm-linux-gnueabihf \
+#       pi@raspberrypi:/usr/include \
+#       pi@raspberrypi:/lib/arm-linux-gnueabihf \
+#       sysroot/
+
+# export LDFLAGS=-fuse-ld=lld
+# cmake .. -GNinja -DCMAKE_TOOLCHAIN_FILE=... -DCMAKE_SYSROOT=...
